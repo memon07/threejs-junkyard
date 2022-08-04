6b9c812d8dbe069a6c5542f5188fd7d700780303
memon07 <memonshoyeb26@gmail.com>
2022-08-04 17:17:34 +0530

    added damping

--- cameras/src/script.js
+++ cameras/src/script.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 /**
  * cursor
@@ -60,6 +61,12 @@ camera.position.z = 3;
 camera.lookAt(mesh.position);
 scene.add(camera);
 
+//Controls
+const controls = new OrbitControls(camera, canvas);
+// controls.target.y = 2;
+// controls.update();
+controls.enableDamping = true;
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
@@ -76,9 +83,13 @@ const tick = () => {
   //   mesh.rotation.y = elapsedTime;
 
   //update camera
-  camera.position.x = cursor.x * 13;
-  camera.position.y = cursor.y * 13;
-  camera.lookAt(mesh.position);
+  //   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  //   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  //   camera.position.y = cursor.y * 3;
+  //   camera.lookAt(mesh.position);
+
+  //update controls
+  controls.update();
 
   // Render
   renderer.render(scene, camera);
